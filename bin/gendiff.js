45349c54c24c761c02a4426f53e7a9dc765f2063
Evgeniy Rassolka <johnrass@yandex.ru>
2023-03-21 16:14:25 +0300

    step 2: add options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'display help for command');
-
-program.parse();
+  .helpOption('-h, --help', 'display help for command')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .parse(process.argv);
