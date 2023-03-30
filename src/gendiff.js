69cbaa1b0de012e0c6d9a38725a9a84e3a531331
Evgeniy Rassolka <johnrass@yandex.ru>
2023-03-30 08:06:04 +0300

    step 3: add check for absolute path

--- src/gendiff.js
+++ src/gendiff.js
@@ -4,8 +4,14 @@ import _ from 'lodash';
 
 const genDiff = (filepath1, filepath2) => {
   const getFileData = (filepath) => {
-    const extname = path.extname(filepath);
-    const data = fs.readFileSync(filepath, 'utf-8');
+    const fullPath = path.isAbsolute(filepath) ? filepath : path.resolve(process.cwd(), filepath);
+    
+    if (!fs.existsSync(fullPath)) {
+      throw new Error(`File not found: ${fullPath}`);
+    }
+
+    const extname = path.extname(fullPath);
+    const data = fs.readFileSync(fullPath, 'utf-8');
     switch (extname) {
       case '.json':
         return JSON.parse(data);
@@ -16,7 +22,7 @@ const genDiff = (filepath1, filepath2) => {
 
   const obj1 = getFileData(filepath1);
   const obj2 = getFileData(filepath2);
-  const keys = _.union(Object.keys(obj1), Object.keys(obj2)).sort();
+  const keys = Object.keys({ ...obj1, ...obj2 });
 
   const diff = keys.map((key) => {
     if (!_.has(obj1, key)) {
